8de3e5df790465e59ced35f27468e6233bc98434
anahidzandi <67750609+anahidzandi@users.noreply.github.com>
2022-03-11 15:30:19 -0800

    Basic Setup

---
+++ Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    public Transform lookAt;
+    public float boundX = 0.3f;
+    public float boundY = 0.15f;
+
+    private void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+
+        //Check inside bounds for x-axis
+        float deltaX = lookAt.position.x - transform.position.x;
+        if(deltaX > boundX || deltaX < -boundX)
+        {
+            if(transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+            }
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+
+        //Check inside bounds for y-axis
+        float deltaY = lookAt.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY)
+        {
+            if (transform.position.y < lookAt.position.y)
+            {
+                delta.y = deltaY - boundY;
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+        //Move camera
+        transform.position += new Vector3(delta.x, delta.y, 0);
+    }
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+    private RaycastHit2D hit;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        //Get Input
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        //Debug.Log(x);
+        //Debug.Log(y);
+
+        //Reset moveDelta
+        moveDelta = new Vector3(x,y,0);
+
+        //Swap sprite direction
+        if (moveDelta.x > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        //y-collision
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+
+        if (hit.collider == null)
+        {
+            //Movement
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        //x-collision
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+
+        if (hit.collider == null)
+        {
+            //Movement
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
